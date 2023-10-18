c9b0bc69a55ddd4b42e36321fd69c2c60131e181
Iskandar <you@example.com>
2023-10-18 23:03:54 +0500

    Foods project xatolik home da

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView, DeleteView, DetailView, View
+from main.models import Menu, Home
+
+class Home(TemplateView):
+    model = Home
+    template_name = 'main/home.html'
+
+# class Menu(ListView) :
+#     model = Menu
+#     template_name = 'main/menu.html'
